918f82059593d4380f41e0c7e1fdf640ea3068b4
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-27 16:56:18 -0300

    style form new user

--- src/components/ModalNewUser/index.tsx
+++ src/components/ModalNewUser/index.tsx
@@ -1,22 +1,71 @@
-import { RdInput, RdModal } from "readmedev-ds";
+import "./ModalNewUser.css";
+
+import { RdButton, RdInput, RdModal } from "readmedev-ds";
 import loginImage from "./assets/login.png";
 import { useState } from "react";
 
 const ModalNewUser = () => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [addressPlus, setAddressPlus] = useState("");
+  const [cep, setCep] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
-    <RdModal title="Cadastrar" open={true} onClose={() => {}}>
-      <figure>
-        <img src={loginImage} alt="Monitor com formulário de cadastro" />
-      </figure>
-      <form action="">
-        <RdInput
-          label="Nome"
-          value={name}
-          onChange={setName}
-          shadowEffect={false}
-        />
-      </form>
+    <RdModal title="Cadastro de Usuário" open={true} onClose={() => {}}>
+      <div className="container-modal">
+        <form action="">
+          <RdInput
+            label="Nome"
+            value={name}
+            onChange={setName}
+            shadowEffect={false}
+          />
+          <RdInput
+            label="E-mail"
+            value={email}
+            onChange={setEmail}
+            shadowEffect={false}
+          />
+          <RdInput
+            label="Endereço"
+            value={address}
+            onChange={setAddress}
+            shadowEffect={false}
+          />
+          <RdInput
+            label="Complemento"
+            value={addressPlus}
+            onChange={setAddressPlus}
+            shadowEffect={false}
+          />
+          <RdInput
+            label="CEP"
+            value={cep}
+            onChange={setCep}
+            shadowEffect={false}
+          />
+          <RdInput
+            label="Senha"
+            type="password"
+            value={password}
+            onChange={setPassword}
+            shadowEffect={false}
+          />
+          <RdInput
+            label="Confirmar Senha"
+            type="password"
+            value={confirmPassword}
+            onChange={setConfirmPassword}
+            shadowEffect={false}
+          />
+          <footer>
+            <RdButton text="Cadastrar" onClick={() => {}} btnType="secondary" />
+          </footer>
+        </form>
+      </div>
     </RdModal>
   );
 };
