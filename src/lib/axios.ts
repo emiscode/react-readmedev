8f145e9666c6adc2f9b6617ffe586050bf3ec311
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-12-08 08:37:54 -0300

    add axios interceptor to handle headers with token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,10 +6,33 @@ const api = (token?: string | null) => {
     baseURL: env?.SERVER_API_URL || "http://localhost:8000",
     headers: token
       ? {
+          Accept: "application/json",
+          Content: "application/json",
           Authorization: `Bearer ${token}`,
         }
-      : {},
+      : { Accept: "application/json", Content: "application/json" },
   });
 };
+
+const http = api();
+
+/* alternative way to insert token into each request */
+http.interceptors.request.use(
+  function (config) {
+    const token = sessionStorage.getItem("token");
+
+    if (token && config.headers) {
+      if (!config.headers.Authorization) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+  function (error) {
+    console.log("Erro no interceptor do axios", error);
+    return Promise.reject(error);
+  }
+);
 
 export default api;
