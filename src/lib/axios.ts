d39da9dcd2e2c72d924d77b2821711e82ff90379
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-12-07 22:57:06 -0300

    configure axios headers and render orders on logged area

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import env from "react-dotenv";
 
-const api = () => {
+const api = (token?: string | null) => {
   return axios.create({
     baseURL: env.SERVER_API_URL,
+    headers: token
+      ? {
+          Authorization: `Bearer ${token}`,
+        }
+      : {},
   });
 };
 
--- src/pages/LoggedArea/Pedidos/index.tsx
+++ src/pages/LoggedArea/Pedidos/index.tsx
@@ -1,64 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RdButton } from "readmedev-ds";
+import { useGetToken } from "../../../hooks/token";
+import { IPedido } from "../../../interfaces/IPedido";
+import api from "../../../lib/axios";
 import "./Pedidos.css";
 
 const Pedidos = () => {
-  useEffect(() => {}, []);
+  const token = useGetToken();
+  const [pedidos, setPedidos] = useState<IPedido[]>([]);
+
+  const currencyFormatter = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  useEffect(() => {
+    api(token)
+      .get<IPedido[]>("pedidos")
+      .then((response) => {
+        setPedidos(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [token]);
 
   return (
     <section className="pedidos">
       <h1>Meus pedidos</h1>
-      <div className="pedido_container">
-        <ul>
-          <li>
-            Pedido: <strong>123</strong>
-          </li>
-          <li>
-            Data do pedido: <strong>05/12/2022</strong>
-          </li>
-          <li>
-            Valor total: <strong>R$ 29,90</strong>
-          </li>
-          <li>
-            Entrega realizada em : <strong>06/12/2022</strong>
-          </li>
-        </ul>
-        <RdButton text="Detalhes" />
-      </div>
-      <div className="pedido_container">
-        <ul>
-          <li>
-            Pedido: <strong>123</strong>
-          </li>
-          <li>
-            Data do pedido: <strong>05/12/2022</strong>
-          </li>
-          <li>
-            Valor total: <strong>R$ 29,90</strong>
-          </li>
-          <li>
-            Entrega realizada em : <strong>06/12/2022</strong>
-          </li>
-        </ul>
-        <RdButton text="Detalhes" />
-      </div>
-      <div className="pedido_container">
-        <ul>
-          <li>
-            Pedido: <strong>123</strong>
-          </li>
-          <li>
-            Data do pedido: <strong>05/12/2022</strong>
-          </li>
-          <li>
-            Valor total: <strong>R$ 29,90</strong>
-          </li>
-          <li>
-            Entrega realizada em : <strong>06/12/2022</strong>
-          </li>
-        </ul>
-        <RdButton text="Detalhes" />
-      </div>
+      {pedidos.map((pedido) => (
+        <div className="pedido-container" key={pedido.id}>
+          <ul>
+            <li>
+              Pedido: <strong>{pedido.id}</strong>
+            </li>
+            <li>
+              Data do pedido:{" "}
+              <strong>{new Date(pedido.data).toLocaleDateString()}</strong>
+            </li>
+            <li>
+              Valor total:{" "}
+              <strong>{currencyFormatter.format(pedido.total)}</strong>
+            </li>
+            <li>
+              Entrega realizada em :{" "}
+              <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong>
+            </li>
+          </ul>
+          <RdButton text="Detalhes" />
+        </div>
+      ))}
     </section>
   );
 };
